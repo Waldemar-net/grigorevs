1288cb67913ee8b6c57df889879dd02b454f3e53
waldemar-net <waldemar-net@yandex.ru>
2024-03-20 10:49:00 +0300

    New Curse

--- grigorevs/main/views.py
+++ grigorevs/main/views.py
@@ -3,8 +3,15 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    
-    return render(request, 'main/index.html')
+    context = {
+        'title':'Home',
+        'content': 'Главная страница магазина - grigorevs',
+        'list' : ['first', 'second'],
+        'dict' : {'first':1},
+        'is_authenticated': False
+    }
+    return render(request, 'main/index.html', context)
 
 def about(request):
     return HttpResponse('About page')
+ 
